974af5cbf0e79ae1361c5edf83a80bcf3f00efef
Jon Pretty <jon.pretty@propensive.com>
2023-11-30 14:30:13 +0100

    Progress on front-end

--- src/model/fury.scala
+++ src/model/fury.scala
@@ -58,7 +58,6 @@ given (using CanThrow[AppError]): Raises[AggregateError[Error]] =
 enum BusMessage:
   case Ping
 
-val logo: Text = t"CiAgICAgICAgICAgICAgIBtbMzg7MjsyMTc7MTsxMDFtwrcg4oCiIOKXjyAbWzM4OzI7MjA7NjA7MTAwbeKXjyDil48g4oCiIMK3CiAgICAgICAgIBtbMzg7MjsyMTc7MTsxMDFtwrcg4pePIOKXjyAbWzM4OzI7MTEyOzQ7NDNt4pePIBtbMzg7MjsxODs1Njs5NW3il48g4pePIOKXjyDil48gG1szODsyOzgzOzQ5OzJt4pePIBtbMzg7MjsxODs1Njs5NW3il48g4pePIOKXjyDCtwogICAgICAgIBtbMzg7MjsyMjc7MTs5MW3igKIg4pePIOKXjyAbWzM4OzI7MTEyOzQ7NDNt4pePIBtbMzg7MjsxODs1Njs5NW3il48g4pePIOKXjyAbWzM4OzI7MjEwOzExNTs3beKXjyDil48gG1szODsyOzA7MzM7MzZt4pePIBtbMzg7MjsxODs1Njs5NW3il48g4pePIOKXjyDigKIgICAgICAgIBtbMzg7MjsxNTA7MTUwOzE1MG3ila3ilIDilIDilIDilIDilIDila4KICAgICAgIBtbMzg7MjsyMjc7MTs5MW3il48g4pePIOKXjyDil48gG1szODsyOzE2OzUyOzkwbeKXjyAbWzM4OzI7ODM7NDk7Mm3il48gG1szODsyOzIxMDsxMTU7N23il48g4pePIOKXjyAbWzM4OzI7MDszMzszNm3il48gG1szODsyOzE2OzUyOzkwbeKXjyDil48g4pePIOKXjyDil48gICAgICAgG1szODsyOzE1MDsxNTA7MTUwbeKUgiAg4pWt4pSA4pSA4pWvCiAgICAgIBtbMzg7MjsyMjc7MTs5MW3il48g4pePIOKXjyDil48gG1szODsyOzgzOzQ5OzJt4pePIBtbMzg7MjsyMTA7MTE1Ozdt4pePIOKXjyDil48g4pePIBtbMzg7MjswOzMzOzM2beKXjyAbWzM4OzI7MTY7NTI7OTBt4pePIOKXjyAbWzM4OzI7Njg7NzY7MjZt4pePIBtbMzg7MjsxNjs1Mjs5MG3il48g4pePIOKXjyAgICAgIBtbMzg7MjsxNTA7MTUwOzE1MG3ilIIgIOKUggogICAgIBtbMzg7MjsyMzc7MTs4MW3igKIg4pePIOKXjyDil48gG1szODsyOzIyODsxMDQ7OW3il48gG1szODsyOzIxMDsxMTU7N23il48g4pePIOKXjyDil48gG1szODsyOzA7MzM7MzZt4pePIBtbMzg7MjsxNDs0ODs4NW3il48g4pePIBtbMzg7MjsxNzA7MTY4OzMxbeKXjyDil48gG1szODsyOzE0OzQ4Ozg1beKXjyDil48g4oCiICAgICAbWzM4OzI7MTUwOzE1MDsxNTBt4pSCICDilJTilIDilIDila7ila3ilIDilIDila4g4pWt4pSA4pSA4pWu4pWt4pSA4pSA4pWu4pSA4pSA4pSA4pWu4pWt4pSA4pSA4pWuIOKVreKUgOKUgOKVrgogICAgG1szODsyOzIzNzsxOzgxbcK3IOKXjyDil48gG1szODsyOzIxMTs0NDsxN23il48gG1szODsyOzIyODsxMDQ7OW3il48g4pePIBtbMzg7MjsyMTA7MTE1Ozdt4pePIOKXjyDil48gG1szODsyOzgzOzQ5OzJt4pePIBtbMzg7MjsxNDs0ODs4NW3il48gG1szODsyOzE3MDsxNjg7MzFt4pePIOKXjyAbWzM4OzI7Njg7NzY7MjZt4pePIBtbMzg7MjsxNDs0ODs4NW3il48g4pePIOKXjyDCtyAgICAbWzM4OzI7MTUwOzE1MDsxNTBt4pSCICDilIzilIDilIDila/ilIIgIOKUgiDilIIgIOKUguKUgiAg4pWt4pSA4pSA4pSA4pWv4pSCICDilIIg4pSCICDilIIKICAgICAbWzM4OzI7MjM3OzE7ODFt4pePIOKXjyAbWzM4OzI7MjI4OzEwNDs5beKXjyDil48g4pePIOKXjyAbWzM4OzI7MjEwOzExNTs3beKXjyDil48gG1szODsyOzIzMDsxNzU7OG3il48gG1szODsyOzE3MDsxNjg7MzFt4pePIOKXjyDil48gG1szODsyOzY4Ozc2OzI2beKXjyAbWzM4OzI7MTI7NDQ7ODBt4pePIOKXjyDil48g4pePICAgICAbWzM4OzI7MTUwOzE1MDsxNTBt4pSCICDilIIgICDilIIgIOKUgiDilIIgIOKUguKUgiAg4pSCICAgIOKUgiAg4pSCIOKUgiAg4pSCCiAgICAbWzM4OzI7MjQ3OzE7NzFtwrcg4pePIBtbMzg7MjsyMjg7MTA0Ozlt4pePIOKXjyDil48g4pePIOKXjyAbWzM4OzI7MjQ3OzE4MTsyNG3il48g4pePIBtbMzg7MjsxNzA7MTY4OzMxbeKXjyDil48g4pePIBtbMzg7Mjs2ODs3NjsyNm3il48gG1szODsyOzEyOzQ0OzgwbeKXjyDil48g4pePIOKXjyDCtyAgICAbWzM4OzI7MTUwOzE1MDsxNTBt4pSCICDilIIgICDilIIgIOKVsOKUgOKVryAg4pSC4pSCICDilIIgICAg4pSCICDilbDilIDila8gIOKUggogICAgIBtbMzg7MjsyNDc7MTs3MW3igKIgG1szODsyOzIxMTs0NDsxN23il48gG1szODsyOzIyODsxMDQ7OW3il48g4pePIOKXjyAbWzM4OzI7MjQ3OzE4MTsyNG3il48g4pePIOKXjyDil48gG1szODsyOzE3MDsxNjg7MzFt4pePIOKXjyAbWzM4OzI7Njg7NzY7MjZt4pePIBtbMzg7MjsxMDs0MDs3NW3il48g4pePIOKXjyDil48g4oCiICAgICAbWzM4OzI7MTUwOzE1MDsxNTBt4pWw4pSA4pSA4pWvICAg4pWw4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pWv4pWw4pSA4pSA4pWvICAgIOKVsOKUgOKUgOKUgOKUgOKUkCAg4pSCCiAgICAgIBtbMzg7MjsyNDc7MTs3MW3il48gG1szODsyOzIyODsxMDQ7OW3il48g4pePIBtbMzg7MjsyMzA7MTc1Ozht4pePIBtbMzg7MjsyNDc7MTgxOzI0beKXjyDil48g4pePIOKXjyAbWzM4OzI7MTcwOzE2ODszMW3il48g4pePIBtbMzg7MjswOzMzOzM2beKXjyAbWzM4OzI7MTA7NDA7NzVt4pePIOKXjyDil48g4pePIOKXjyAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgG1szODsyOzE1MDsxNTA7MTUwbeKUgiAg4pSCCiAgICAgICAbWzM4OzI7MjI4OzEwNDs5beKXjyDil48gG1szODsyOzIzMDsxNzU7OG3il48gG1szODsyOzI0NzsxODE7MjRt4pePIOKXjyDil48g4pePIOKXjyAbWzM4OzI7MTcwOzE2ODszMW3il48gG1szODsyOzA7MzM7MzZt4pePIBtbMzg7Mjs4OzM2OzcwbeKXjyDil48g4pePIOKXjyDil48gICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgG1szODsyOzE1MDsxNTA7MTUwbeKVreKUgOKUgOKUgOKUgOKVryAg4pSCCiAgICAgICAgG1szODsyOzIyODsxMDQ7OW3igKIg4pePIBtbMzg7MjsyMzA7MTc1Ozht4pePIBtbMzg7MjsyNDc7MTgxOzI0beKXjyDil48g4pePIOKXjyAbWzM4OzI7MTcwOzE2ODszMW3il48gG1szODsyOzg7MzY7NzBt4pePIOKXjyDil48g4pePIOKXjyDigKIgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIBtbMzg7MjsxNTA7MTUwOzE1MG3ilbDilIDilIDilIDilIDilIDilIDilIDila8KICAgICAgICAgG1szODsyOzIyODsxMDQ7OW3CtyAbWzM4OzI7MjQ3OzE4MTsyNG3il48g4pePIOKXjyDil48g4pePIOKXjyAbWzM4OzI7NjszMjs2NW3il48g4pePIOKXjyDil48g4pePIMK3CiAgICAgICAgICAgICAgIBtbMzg7MjsyNDc7MTgxOzI0bcK3IOKAoiDil48gG1szODsyOzE1MDsxMTc7MzRt4pePIBtbMzg7Mjs0OzI4OzYwbeKXjyDigKIgwrcKG1swbQo=".decode[Base64].uString
 
 given Realm = realm"fury"
 
@@ -76,13 +75,10 @@ object userInterface:
   val Init = Subcommand(t"init", e"Initialize a new project")
   val Universe = Subcommand(t"universe", e"Universe actions")
   val Update = Subcommand(t"update", e"Update Fury")
-  val Vent = Subcommand(t"vent", e"Access the Vent Ecosystem")
   val Install = Subcommand(t"install", e"Install Fury")
 
   val Clean = Subcommand(t"clean", e"Clean the cache")
   val Details = Subcommand(t"info", e"Information about cache usage")
-
-
 
 @main
 def main(): Unit =
@@ -93,107 +89,93 @@ def main(): Unit =
       given Log[Output] = logging.silent[Output]
 
       daemon[BusMessage]:
-        if !Version().unset then
-          execute:
-            Out.println(e"$Bold(Fury, version 1.0)")
-            ExitStatus.Ok
-        
-        else safely(arguments.head) match
-          case Install() =>
-            val interactive = !Interactive().unset
-            val noTabCompletions = !NoTabCompletions().unset
+        try throwErrors[UserError]:
+          given HomeDirectory =
+            mitigate:
+              case SystemPropertyError(property) =>
+                UserError(msg"""Could not work out home directory because the $property system property was not
+                    set""")
+            .within(homeDirectory.jvm)
+          
+          given WorkingDirectory =
+            mitigate:
+              case SystemPropertyError(property) =>
+                UserError(msg"""Could not discover the working directory because the $property system property
+                    was not set""")
+            .within(workingDirectories.daemonClient)
+
+          if !Version().unset then execute(versionInfo())
+          else safely(arguments.head) match
+            case Install() =>
+              val interactive = !Interactive().unset
+              val noTabCompletions = !NoTabCompletions().unset
+              
+              execute:
+                try throwErrors[UserError]:
+                  import logging.pinned
+                  
+                  if interactive then terminal(installInteractive(noTabCompletions))
+                  else installBatch(noTabCompletions)
+                  
+                  service.shutdown()
+                  ExitStatus.Ok
+                
+                catch
+                  case userError: UserError =>
+                    Out.println(userError.message)
+                    ExitStatus.Fail(1)
+
+            case Init() => execute(initializeBuild())
+
+            case Cache() => safely(arguments.tail.head) match
+              case Clean()   => execute(cleanCache())
+              case Details() => execute(cacheDetails())
+              case other     => execute(other.mm(invalidSubcommand(_)).or(missingSubcommand()))
+
+            case About() => execute(about())
+            case Build() => execute(runBuild())
             
-            execute:
-              import workingDirectories.daemonClient
-              import unsafeExceptions.canThrowAny
-              import logging.pinned
-              
-              throwErrors[ExecError | NotFoundError | DismissError | EnvironmentError | NumberError | SystemPropertyError | PathError | IoError | StreamCutError | OverwriteError]:
-                if interactive then terminal:
-                  val directories = Installer.candidateTargets().map(_.path)
-                  if directories.length <= 1 then Installer.install()
-                  else
-                    Out.println(e"$Italic(Please choose an install location.)")
-                    val menu = SelectMenu(directories, directories.head)
-                    val (target, events2) = menu.ask(tty.events)
-                    Out.println(e"Installing to $target/${service.scriptName}")
-                    Out.println(Installer.install(force = true, target).communicate)
-                    if !noTabCompletions then Out.println(TabCompletions.install(force = true).communicate)
-                else
-                  Out.println(Installer.install().communicate)
-                  if !noTabCompletions then Out.println(TabCompletions.install(force = true).communicate)
-              
+            case Shutdown() => execute:
               service.shutdown()
               ExitStatus.Ok
-
-          case Init() => execute:
-            Out.println(t"Creating a new build")
-            ExitStatus.Ok
-
-          case Vent() => execute:
-            Out.println(e"${colors.Goldenrod}(Vent your Fury!)")
-            ExitStatus.Ok
-
-          case Cache() => safely(arguments.tail.head) match
-            case Clean() =>
-              execute:
-                Out.println(t"Clean the cache")
-                ExitStatus.Ok
             
-            case Details() =>
-              execute:
-                Out.println(t"Details of the cache")
-                ExitStatus.Ok
-
-            case other =>
-              execute:
-                Out.println(t"Not a valid subcommand")
-                ExitStatus.Fail(1)
-
-          case About() => execute:
-            Out.println(logo)
-            ExitStatus.Ok
-          
-          case Build() => execute:
-            Out.println(t"Running the build")
-            ExitStatus.Ok
-          
-          case Shutdown() => execute:
-            service.shutdown()
-            ExitStatus.Ok
-          
-          case Universe() => execute:
-            import unsafeExceptions.canThrowAny
-            throwErrors[PathError | SystemPropertyError | CancelError | HostnameError | CodlReadError | UrlError |
-                GitRefError | StreamCutError | IoError | InvalidRefError | NumberError | NotFoundError | GitError |
-                UndecodableCharError | UnencodableCharError | MarkdownError | ExecError | DateError]:
-              internet:
-                frontEnd:
-                  given installation: Installation = Installation((Xdg.cacheHome[Path] / p"fury").as[Directory])
-                  val rootWorkspace = Workspace(Properties.user.dir())
-                  given universe: Universe = rootWorkspace.universe()
-                  val projects = universe.projects.to(List)
-                  
-                  // val terminfo = sh"infocmp -0 -L -q -t ${Environment.term}".exec[Text]().cut(t",").
-                  // Out.println(terminfo.debug)
-
-                  terminal:
-                    Async(tty.events.foreach(_ => ()))
+            case Universe() => execute:
+              import unsafeExceptions.canThrowAny
+              throwErrors[PathError | SystemPropertyError | CancelError | HostnameError | CodlReadError | UrlError |
+                  GitRefError | StreamCutError | IoError | InvalidRefError | NumberError | NotFoundError | GitError |
+                  UndecodableCharError | UnencodableCharError | MarkdownError | ExecError | DateError]:
+                internet:
+                  frontEnd:
+                    given installation: Installation = Installation((Xdg.cacheHome[Path] / p"fury").as[Directory])
+                    val rootWorkspace = Workspace(Properties.user.dir())
+                    given universe: Universe = rootWorkspace.universe()
+                    val projects = universe.projects.to(List)
                     
-                    Table[(ProjectId, Definition)](
-                      Column(e"$Bold(Project ID)")(_(0)),
-                      Column(e"$Bold(Name)")(_(1).name),
-                      Column(e"$Bold(Description)")(_(1).description),
-                      Column(e"$Bold(Website)")(_(1).website.mm(_.show).or(t"—")),
-                      Column(e"$Bold(Source)"): (_, definition) =>
-                        definition.source match
-                          case workspace: Workspace => e"$Aquamarine(${rootWorkspace.directory.path.relativeTo(workspace.directory.path)})"
-                          case vault: Vault         => e"$SeaGreen(${vault.name})"
-                    ).tabulate(projects, tty.knownColumns, DelimitRows.SpaceIfMultiline).foreach(Out.println(_))
+                    // val terminfo = sh"infocmp -0 -L -q -t ${Environment.term}".exec[Text]().cut(t",").
+                    // Out.println(terminfo.debug)
 
-                    ExitStatus.Ok
-          
-          case subcommand =>
+                    terminal:
+                      Async(tty.events.foreach(_ => ()))
+                      
+                      Table[(ProjectId, Definition)](
+                        Column(e"$Bold(Project ID)")(_(0)),
+                        Column(e"$Bold(Name)")(_(1).name),
+                        Column(e"$Bold(Description)")(_(1).description),
+                        Column(e"$Bold(Website)")(_(1).website.mm(_.show).or(t"—")),
+                        Column(e"$Bold(Source)"): (_, definition) =>
+                          definition.source match
+                            case workspace: Workspace => e"$Aquamarine(${rootWorkspace.directory.path.relativeTo(workspace.directory.path)})"
+                            case vault: Vault         => e"$SeaGreen(${vault.name})"
+                      ).tabulate(projects, tty.knownColumns, DelimitRows.SpaceIfMultiline).foreach(Out.println(_))
+
+                      ExitStatus.Ok
+            
+            case subcommand =>
+              execute:
+                Out.println(t"Unrecognized subcommand: ${subcommand.mm(_()).or(t"")}.")
+                ExitStatus.Fail(1)
+        catch
+          case userError: UserError =>
             execute:
-              Out.println(t"Unrecognized subcommand: ${subcommand.mm(_()).or(t"")}.")
+              Out.println(userError.message)
               ExitStatus.Fail(1)
---
+++ src/model/ui.scala
@@ -0,0 +1,81 @@
+package fury
+
+import rudiments.*
+import profanity.*
+import turbulence.*
+import ambience.*
+import exoskeleton.*
+import fulminate.*
+import galilei.*
+import gastronomy.*
+import gossamer.*
+import anticipation.*
+import eucalyptus.*
+import spectral.*
+import escapade.*
+import perforate.*
+
+case class UserError(userMessage: Message) extends Error(userMessage)
+
+def installInteractive
+    (noTabCompletions: Boolean)
+    (using DaemonService[?], Terminal, Log[Output], SystemProperties, Environment, WorkingDirectory,
+        HomeDirectory, Effectful)
+    : ExitStatus raises UserError =
+  mitigate:
+    case InstallError(reason) => UserError(msg"Installation was not possible because $reason.")
+    case DismissError()       => UserError(msg"Installation was aborted.")
+  .within:
+    val directories = Installer.candidateTargets().map(_.path)
+    if directories.length <= 1 then Installer.install()
+    else
+    Out.println(e"$Italic(Please choose an install location.)")
+    val menu = SelectMenu(directories, directories.head)
+    val (target, events2) = menu.ask(tty.events)
+    Out.println(e"Installing to $target/${service.scriptName}")
+    Out.println(Installer.install(force = true, target).communicate)
+    if !noTabCompletions then Out.println(TabCompletions.install(force = true).communicate)
+    ExitStatus.Ok
+
+def installBatch
+    (noTabCompletions: Boolean)
+    (using DaemonService[?], Stdio, Log[Output], SystemProperties, Environment, WorkingDirectory, HomeDirectory,
+        Effectful)
+    : ExitStatus raises UserError =
+  mitigate:
+    case InstallError(reason) => UserError(msg"Installation was not possible because $reason.")
+  .within:
+    Out.println(Installer.install().communicate)
+    if !noTabCompletions then Out.println(TabCompletions.install(force = true).communicate)
+    ExitStatus.Ok
+
+def initializeBuild()(using Stdio): ExitStatus raises UserError =
+  Out.println(t"Creating a new build")
+  ExitStatus.Ok
+
+def cleanCache()(using Stdio): ExitStatus raises UserError =
+  Out.println(t"Cleaning the cache")
+  ExitStatus.Ok
+
+def cacheDetails()(using Stdio): ExitStatus raises UserError =
+  Out.println(t"Details of the cache")
+  ExitStatus.Ok
+
+def runBuild()(using Stdio): ExitStatus raises UserError =
+  Out.println(t"Running the build...")
+  ExitStatus.Ok
+
+def invalidSubcommand(command: Argument)(using Stdio): ExitStatus raises UserError =
+  abort(UserError(msg"${command()} is not a valid subcommand."))
+
+def missingSubcommand()(using Stdio): ExitStatus raises UserError =
+  abort(UserError(msg"No subcommand was specified."))
+
+def about()(using Stdio): ExitStatus =
+  val logo: Text = t"CiAgICAgICAgICAgICAgIBtbMzg7MjsyMTc7MTsxMDFtwrcg4oCiIOKXjyAbWzM4OzI7MjA7NjA7MTAwbeKXjyDil48g4oCiIMK3CiAgICAgICAgIBtbMzg7MjsyMTc7MTsxMDFtwrcg4pePIOKXjyAbWzM4OzI7MTEyOzQ7NDNt4pePIBtbMzg7MjsxODs1Njs5NW3il48g4pePIOKXjyDil48gG1szODsyOzgzOzQ5OzJt4pePIBtbMzg7MjsxODs1Njs5NW3il48g4pePIOKXjyDCtwogICAgICAgIBtbMzg7MjsyMjc7MTs5MW3igKIg4pePIOKXjyAbWzM4OzI7MTEyOzQ7NDNt4pePIBtbMzg7MjsxODs1Njs5NW3il48g4pePIOKXjyAbWzM4OzI7MjEwOzExNTs3beKXjyDil48gG1szODsyOzA7MzM7MzZt4pePIBtbMzg7MjsxODs1Njs5NW3il48g4pePIOKXjyDigKIgICAgICAgIBtbMzg7MjsxNTA7MTUwOzE1MG3ila3ilIDilIDilIDilIDilIDila4KICAgICAgIBtbMzg7MjsyMjc7MTs5MW3il48g4pePIOKXjyDil48gG1szODsyOzE2OzUyOzkwbeKXjyAbWzM4OzI7ODM7NDk7Mm3il48gG1szODsyOzIxMDsxMTU7N23il48g4pePIOKXjyAbWzM4OzI7MDszMzszNm3il48gG1szODsyOzE2OzUyOzkwbeKXjyDil48g4pePIOKXjyDil48gICAgICAgG1szODsyOzE1MDsxNTA7MTUwbeKUgiAg4pWt4pSA4pSA4pWvCiAgICAgIBtbMzg7MjsyMjc7MTs5MW3il48g4pePIOKXjyDil48gG1szODsyOzgzOzQ5OzJt4pePIBtbMzg7MjsyMTA7MTE1Ozdt4pePIOKXjyDil48g4pePIBtbMzg7MjswOzMzOzM2beKXjyAbWzM4OzI7MTY7NTI7OTBt4pePIOKXjyAbWzM4OzI7Njg7NzY7MjZt4pePIBtbMzg7MjsxNjs1Mjs5MG3il48g4pePIOKXjyAgICAgIBtbMzg7MjsxNTA7MTUwOzE1MG3ilIIgIOKUggogICAgIBtbMzg7MjsyMzc7MTs4MW3igKIg4pePIOKXjyDil48gG1szODsyOzIyODsxMDQ7OW3il48gG1szODsyOzIxMDsxMTU7N23il48g4pePIOKXjyDil48gG1szODsyOzA7MzM7MzZt4pePIBtbMzg7MjsxNDs0ODs4NW3il48g4pePIBtbMzg7MjsxNzA7MTY4OzMxbeKXjyDil48gG1szODsyOzE0OzQ4Ozg1beKXjyDil48g4oCiICAgICAbWzM4OzI7MTUwOzE1MDsxNTBt4pSCICDilJTilIDilIDila7ila3ilIDilIDila4g4pWt4pSA4pSA4pWu4pWt4pSA4pSA4pWu4pSA4pSA4pSA4pWu4pWt4pSA4pSA4pWuIOKVreKUgOKUgOKVrgogICAgG1szODsyOzIzNzsxOzgxbcK3IOKXjyDil48gG1szODsyOzIxMTs0NDsxN23il48gG1szODsyOzIyODsxMDQ7OW3il48g4pePIBtbMzg7MjsyMTA7MTE1Ozdt4pePIOKXjyDil48gG1szODsyOzgzOzQ5OzJt4pePIBtbMzg7MjsxNDs0ODs4NW3il48gG1szODsyOzE3MDsxNjg7MzFt4pePIOKXjyAbWzM4OzI7Njg7NzY7MjZt4pePIBtbMzg7MjsxNDs0ODs4NW3il48g4pePIOKXjyDCtyAgICAbWzM4OzI7MTUwOzE1MDsxNTBt4pSCICDilIzilIDilIDila/ilIIgIOKUgiDilIIgIOKUguKUgiAg4pWt4pSA4pSA4pSA4pWv4pSCICDilIIg4pSCICDilIIKICAgICAbWzM4OzI7MjM3OzE7ODFt4pePIOKXjyAbWzM4OzI7MjI4OzEwNDs5beKXjyDil48g4pePIOKXjyAbWzM4OzI7MjEwOzExNTs3beKXjyDil48gG1szODsyOzIzMDsxNzU7OG3il48gG1szODsyOzE3MDsxNjg7MzFt4pePIOKXjyDil48gG1szODsyOzY4Ozc2OzI2beKXjyAbWzM4OzI7MTI7NDQ7ODBt4pePIOKXjyDil48g4pePICAgICAbWzM4OzI7MTUwOzE1MDsxNTBt4pSCICDilIIgICDilIIgIOKUgiDilIIgIOKUguKUgiAg4pSCICAgIOKUgiAg4pSCIOKUgiAg4pSCCiAgICAbWzM4OzI7MjQ3OzE7NzFtwrcg4pePIBtbMzg7MjsyMjg7MTA0Ozlt4pePIOKXjyDil48g4pePIOKXjyAbWzM4OzI7MjQ3OzE4MTsyNG3il48g4pePIBtbMzg7MjsxNzA7MTY4OzMxbeKXjyDil48g4pePIBtbMzg7Mjs2ODs3NjsyNm3il48gG1szODsyOzEyOzQ0OzgwbeKXjyDil48g4pePIOKXjyDCtyAgICAbWzM4OzI7MTUwOzE1MDsxNTBt4pSCICDilIIgICDilIIgIOKVsOKUgOKVryAg4pSC4pSCICDilIIgICAg4pSCICDilbDilIDila8gIOKUggogICAgIBtbMzg7MjsyNDc7MTs3MW3igKIgG1szODsyOzIxMTs0NDsxN23il48gG1szODsyOzIyODsxMDQ7OW3il48g4pePIOKXjyAbWzM4OzI7MjQ3OzE4MTsyNG3il48g4pePIOKXjyDil48gG1szODsyOzE3MDsxNjg7MzFt4pePIOKXjyAbWzM4OzI7Njg7NzY7MjZt4pePIBtbMzg7MjsxMDs0MDs3NW3il48g4pePIOKXjyDil48g4oCiICAgICAbWzM4OzI7MTUwOzE1MDsxNTBt4pWw4pSA4pSA4pWvICAg4pWw4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pWv4pWw4pSA4pSA4pWvICAgIOKVsOKUgOKUgOKUgOKUgOKUkCAg4pSCCiAgICAgIBtbMzg7MjsyNDc7MTs3MW3il48gG1szODsyOzIyODsxMDQ7OW3il48g4pePIBtbMzg7MjsyMzA7MTc1Ozht4pePIBtbMzg7MjsyNDc7MTgxOzI0beKXjyDil48g4pePIOKXjyAbWzM4OzI7MTcwOzE2ODszMW3il48g4pePIBtbMzg7MjswOzMzOzM2beKXjyAbWzM4OzI7MTA7NDA7NzVt4pePIOKXjyDil48g4pePIOKXjyAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgG1szODsyOzE1MDsxNTA7MTUwbeKUgiAg4pSCCiAgICAgICAbWzM4OzI7MjI4OzEwNDs5beKXjyDil48gG1szODsyOzIzMDsxNzU7OG3il48gG1szODsyOzI0NzsxODE7MjRt4pePIOKXjyDil48g4pePIOKXjyAbWzM4OzI7MTcwOzE2ODszMW3il48gG1szODsyOzA7MzM7MzZt4pePIBtbMzg7Mjs4OzM2OzcwbeKXjyDil48g4pePIOKXjyDil48gICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgG1szODsyOzE1MDsxNTA7MTUwbeKVreKUgOKUgOKUgOKUgOKVryAg4pSCCiAgICAgICAgG1szODsyOzIyODsxMDQ7OW3igKIg4pePIBtbMzg7MjsyMzA7MTc1Ozht4pePIBtbMzg7MjsyNDc7MTgxOzI0beKXjyDil48g4pePIOKXjyAbWzM4OzI7MTcwOzE2ODszMW3il48gG1szODsyOzg7MzY7NzBt4pePIOKXjyDil48g4pePIOKXjyDigKIgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIBtbMzg7MjsxNTA7MTUwOzE1MG3ilbDilIDilIDilIDilIDilIDilIDilIDila8KICAgICAgICAgG1szODsyOzIyODsxMDQ7OW3CtyAbWzM4OzI7MjQ3OzE4MTsyNG3il48g4pePIOKXjyDil48g4pePIOKXjyAbWzM4OzI7NjszMjs2NW3il48g4pePIOKXjyDil48g4pePIMK3CiAgICAgICAgICAgICAgIBtbMzg7MjsyNDc7MTgxOzI0bcK3IOKAoiDil48gG1szODsyOzE1MDsxMTc7MzRt4pePIBtbMzg7Mjs0OzI4OzYwbeKXjyDigKIgwrcKG1swbQo=".decode[Base64].uString
+  Out.println(logo)
+  ExitStatus.Ok
+
+def versionInfo()(using Stdio): ExitStatus = 
+  Out.println(t"Fury version 1.0")
+  ExitStatus.Ok
